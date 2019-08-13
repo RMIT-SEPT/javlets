91acd1b6a633a5749f5c7ee2b08d7c14e3fc54ad
j9d <jamiedimos@gmail.com>
2019-08-13 11:16:00 +1000

    Added backend code

---
+++ backend/src/main/java/com/sept/javlets/JavletsApplication.java
@@ -0,0 +1,13 @@
+package com.sept.javlets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavletsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavletsApplication.class, args);
+	}
+
+}
