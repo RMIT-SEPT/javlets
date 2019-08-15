f4739ee87db5ec2a75014308a772c30d1a3c80c9
j9d <jamiedimos@gmail.com>
2019-08-15 19:33:26 +1000

    Added class for list of posts with filtering methods, can expand later

---
+++ backend/src/main/java/com/sept/javlets/wall/PostList.java
@@ -0,0 +1,44 @@
+package com.sept.javlets.wall;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sept.javlets.userauth.StudentAccountBean;
+
+public class PostList {
+
+	private List<PostBean> posts;
+	
+	public PostList() {
+		this.posts = new ArrayList<PostBean>();
+	}
+	
+	public void addPost(PostBean post) {
+		posts.add(post);
+	}
+	
+	public boolean removePost(PostBean post) {
+		return posts.remove(post);
+	}
+	
+	public List<PostBean> filterAuthor(StudentAccountBean author) {
+		List<PostBean> authorPosts = new ArrayList<PostBean>();
+		for (PostBean post : this.posts) {
+			if (post.getAuthor().equals(author))
+				authorPosts.add(post);
+		}
+		
+		return authorPosts;
+	}
+	
+	public List<PostBean> filterPublic() {
+		List<PostBean> publicPosts = new ArrayList<PostBean>();
+		for (PostBean post : this.posts) {
+			if (post.getPrivacy() == PrivacySetting.PUBLIC)
+				publicPosts.add(post);
+		}
+		
+		return publicPosts;
+	}
+	
+}
