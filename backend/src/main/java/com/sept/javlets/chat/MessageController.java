47fa386ec85b5d3c5a98421c2b6694db8e49ede0
abc123 <schanboth@hotmail.com>
2019-08-16 13:50:26 +1000

    Added todo MessageController - ATT: Need minimal implementation

--- backend/src/main/java/com/sept/javlets/chat/MessageController.java
+++ backend/src/main/java/com/sept/javlets/chat/MessageController.java
@@ -4,5 +4,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
+	
+	/*
+	 * TODO: Store all messages sent in a chatlog
+	 */
+
+	public MessageController() {
+		//TODO
+	}
+	
+	public void newChatlog(MessageBean message) {
+		//TODO
+	}
 
 }
