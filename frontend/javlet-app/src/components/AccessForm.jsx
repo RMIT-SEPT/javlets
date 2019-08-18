1addcb96a3ee171efb8f86879460a98ff235dfc5
“s3569541” <s3569541@student.rmit.edu.au>
2019-08-18 13:24:50 +1000

    updated email input form - still need to send this to back end somehow

--- frontend/javlet-app/src/components/AccessForm.jsx
+++ frontend/javlet-app/src/components/AccessForm.jsx
@@ -34,7 +34,7 @@ class AccessForm extends Component {
     handleSubmit(event) {
         // alert('Users details to be saved: \nName: ' + this.state.name + '\nEmail: ' + this.state.email
         //         + '\nLogin Type: ' + this.state.loginMethod);
-        alert('Email: ' + this.state.email);
+        // alert('Email: ' + this.state.email);
         // event.preventDefault();
     }
   
