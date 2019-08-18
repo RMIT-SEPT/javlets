8d41ff810f4ad8ed693e88e0e9a5c17a3770ee4a
“s3569541” <s3569541@student.rmit.edu.au>
2019-08-18 13:16:28 +1000

    resolved conflicts

--- frontend/javlet-app/src/components/HeaderComponent.jsx
+++ frontend/javlet-app/src/components/HeaderComponent.jsx
@@ -8,7 +8,10 @@ render(){
     <React.Fragment>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <h1 class="navbar-brand title">Javlet</h1>
-  <div>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
       <li class="nav-item active">
           <h2>Insert inspiring slogan here</h2>
