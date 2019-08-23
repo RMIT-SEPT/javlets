4fd8df8ef136b54af1bc3eec5ecd14dff8a4dd03
Jamie <jamiedimos@gmail.com>
2019-08-23 21:32:45 +1000

    Added structure to everything except the test

--- backend/src/test/java/com/sept/javlets/wall/PostTest.java
+++ backend/src/test/java/com/sept/javlets/wall/PostTest.java
@@ -8,10 +8,17 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.sept.javlets.userauth.AccountController;
+
 class PostTest {
 
+	private static PostController postController;
+	private static AccountController accountController;
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		accountController = new AccountController();
+		postController = new PostController(accountController);
 	}
 
 	@AfterAll
@@ -20,10 +27,12 @@ class PostTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		accountController.registerUser("TestUser1");
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		accountController.removeUser("TestUser1");
 	}
 
 	@Test
