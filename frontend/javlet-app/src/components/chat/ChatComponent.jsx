ba2c1eb260aea92b8ec2eaa1c649635d86072671
Justin Vitale <Quadcorenerd@gmail.com>
2019-08-24 22:13:29 +1000

    Debugging

--- frontend/javlet-app/src/components/chat/ChatComponent.jsx
+++ frontend/javlet-app/src/components/chat/ChatComponent.jsx
@@ -6,6 +6,9 @@ class ChatComponent extends Component{
     constructor(props) {
 		super(props);
 		this.state = { 
+			messages: [],  
+			error: '',
+
 			body: '',
 			from: '',
 			to: '',
@@ -14,33 +17,58 @@ class ChatComponent extends Component{
 		this.handleBodyChange = this.handleBodyChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 	  }
+/*
+	  test() {
+		 axios
+		.get(
+		  'http://localhost:8080/chat'
+		)
+		.then(result => {
+		  console.log(result);
+		  const posts = result.data.map(obj => ({type: obj.type, title: obj.title, body: obj.body, author: obj.authorAccount.username, id: obj.id}));
+		  this.setState({ posts });
+		})
+		.catch(error => {
+		  console.error("error: ", error);
+		  this.setState({
+			error: `${error}`
+		  });
+		});
+	}
+	
 
+	componentDidMount(){
+		console.log("I RAN");
+		this.test();
+	  }
+*/
 
-    state = {  }
 render(){
    return(
     <div className="body-item chat">
       <h1>Chat</h1>
+	
 
       <h2>Your connections</h2>
       <ConnectionListComponent type={0} />
       <h2>Add connections</h2>
       <ConnectionListComponent type={1} />
+	  {this.handleSubmit}
       <h2>Message</h2>
 
 
-      <div id="dialogue-page" class="hidden">
-		<div class="dialogue-container">
+      <div id="dialogue-page" className="hidden">
+		<div className="dialogue-container">
 			<ul id="messageList">
 
 			</ul>
 			<form id="dialogueForm" onSubmit={this.handleSubmit} name="dialogueForm" nameForm="dialogueForm">
-				<div class="form-group">
-					<div class="input-group clearfix">
-						<input className="w3-input w3-animate-input" type="text" id="chatMessage"
+				<div className="form-group">
+					<div className="input-group clearfix">
+						<input className="w3-input w3-animate-input form-control" type="text" id="chatMessage"
 							placeholder="Enter a message...." autoComplete="off"
-							class="form-control" onChange={this.handleBodyChange} />
-                <input class="w3-btn w3-blue" type="submit" value="Send" />
+						 onChange={this.handleBodyChange} />
+                <input className="w3-btn w3-blue" type="submit" value="Send" />
 					</div>
 				</div>
 			</form>
@@ -62,14 +90,17 @@ handleBodyChange(event) {
 }
 
 handleSubmit(event) {
+	/*
   const newItem = {
 	body: this.state.body,
 	from: this.state.from,
 	to: this.state.to,
 	id: Date.now()
   };
+  */
+  console.log("handleSubmitted")
 
-  return axios.post('http://localhost:8080/chat/newMessage', newItem);
+  return axios.post('http://localhost:8080/chat/newMessage');
 }
 
 }
