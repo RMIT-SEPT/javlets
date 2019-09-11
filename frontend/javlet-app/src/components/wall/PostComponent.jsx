d6611b6b0abfbb3d239cf478d8172cf86002d89a
“s3569541” <s3569541@student.rmit.edu.au>
2019-09-11 19:36:50 +1000

    added ability to filter posts by mentor or student

--- frontend/javlet-app/src/components/wall/PostComponent.jsx
+++ frontend/javlet-app/src/components/wall/PostComponent.jsx
@@ -5,10 +5,14 @@ class PostComponent extends Component{
   constructor(props) {
     super(props)
     this.state = {
-        posts: [],  
+        allPosts: [],
+        mentorPosts: [],
+        studentPosts: [],
+        postType: 'allPosts',
         error: ''
     };
     this.refreshWall = this.refreshWall.bind(this)
+    this.handlePostTypeChange = this.handlePostTypeChange.bind(this);
   }
 
   componentDidMount(){
@@ -21,9 +25,19 @@ class PostComponent extends Component{
         'http://localhost:8080/wall'
       )
       .then(result => {
-        console.log(result);
-        const posts = result.data.map(obj => ({type: obj.type, title: obj.title, body: obj.body, author: obj.authorAccount.username, id: obj.id}));
-        this.setState({ posts });
+          console.log(result);
+          const allPosts = result.data.reverse().map(obj => ({type: obj.type, title: obj.title, body: obj.body, author: obj.authorAccount.username, id: obj.id}));
+          const mentorPosts = allPosts.filter(function(type){
+            return type.type === "Mentor";
+          })
+          const studentPosts = allPosts.filter(function(type){
+            return type.type === "Student";
+          })
+          // console.log("!!!")
+          // console.log(studentPosts)
+          this.setState({ allPosts });
+          this.setState({ mentorPosts });
+          this.setState({ studentPosts });
       })
       .catch(error => {
         console.error("error: ", error);
@@ -31,12 +45,25 @@ class PostComponent extends Component{
           error: `${error}`
         });
       });
+      
   }
 
   render(){
+    var displayPosts = this.state.allPosts;
+    if(this.state.postType==="Mentor"){
+      displayPosts = this.state.mentorPosts;
+    }
+    else if(this.state.postType==="Student"){
+      displayPosts = this.state.studentPosts;
+    }
     return(
-     <div>
-        {this.state.posts.reverse().map(item => (
+      <div>
+        <h2>Filter Posts:</h2>
+        <input className="w3-radio" type="radio" value="Mentor"  name="formSelect" onClick={this.handlePostTypeChange} /> Display Mentor Posts <br />
+        <input className="w3-radio" type="radio" value="Student" name="formSelect" onClick={this.handlePostTypeChange} /> Display Student Posts<br />
+        <input className="w3-radio" type="radio" value="All" name="formSelect" onClick={this.handlePostTypeChange} /> Display All Posts<br />
+        <br />
+        {displayPosts.map(item => (  
           <div className="post" key={item.id}>
               <h2>{item.title}</h2>
               <p>{item.body}</p>
@@ -48,6 +75,10 @@ class PostComponent extends Component{
     );
   }
 
+  handlePostTypeChange(event) {
+    this.setState({ postType: event.target.value });
+    console.log(this.state.mentorPosts)
+  }
 }
 
 export default PostComponent;
