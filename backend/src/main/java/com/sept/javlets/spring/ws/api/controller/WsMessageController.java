fe5b3630d45a63ef5bbddd55a3f12f22fb71a869
abc123 <schanboth@hotmail.com>
2019-09-11 22:22:19 +1000

    Restarted on websocket

---
+++ backend/src/main/java/com/sept/javlets/spring/ws/api/controller/WsMessageController.java
@@ -0,0 +1,27 @@
+package com.sept.javlets.spring.ws.api.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+import com.sept.javlets.spring.ws.api.model.WsMessage;
+
+@Controller
+public class WsMessageController {
+	
+	@MessageMapping("/chat.register")
+	@SendTo("/topic/public")
+	public WsMessage register(@Payload WsMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+		headerAccessor.getSessionAttributes().put("Username", chatMessage.getSender());
+		return chatMessage;
+	}
+	
+	@MessageMapping("/chat.send")
+	@SendTo("/topic/public")
+	public WsMessage sendmesageMessage(@Payload WsMessage chatMessage) {
+		return chatMessage;
+	}
+	
+}
