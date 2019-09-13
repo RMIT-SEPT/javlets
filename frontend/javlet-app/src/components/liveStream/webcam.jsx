8b0c20149ec1f63994e22d3bb63e196518c79900
Joseph Williams <s3569541@student.rmit.edu.au>
2019-09-13 15:34:44 +1000

    fixing conflict and unused variables in webcam

--- frontend/javlet-app/src/components/liveStream/webcam.jsx
+++ frontend/javlet-app/src/components/liveStream/webcam.jsx
@@ -4,37 +4,37 @@ import NewWindow from 'react-new-window'
 
 // const WebcamComponent = () => <Webcam />;
 
-const videoConstraints = {
-    width: 1280,
-    height: 720,
-    facingMode: "user"
-  };
+// const videoConstraints = {
+//     width: 1280,
+//     height: 720,
+//     facingMode: "user"
+//   };
    
-const WebcamCapture = () => {
-  const webcamRef = React.useRef(null);
+// const WebcamCapture = () => {
+//   const webcamRef = React.useRef(null);
 
-  const capture = React.useCallback(
-      () => {
-      const imageSrc = webcamRef.current.getScreenshot();
-      },
-      [webcamRef]
-  );
+//   const capture = React.useCallback(
+//       () => {
+//       const imageSrc = webcamRef.current.getScreenshot();
+//       },
+//       [webcamRef]
+//   );
 
-  return (
-      <>      
-      <Webcam
-          audio={false}
-          height={720}
-          ref={webcamRef}
-          screenshotFormat="image/jpeg"
-          width={1280}
-          videoConstraints={videoConstraints}
-      />
-      <button onClick={capture}>Capture photo</button>
+//   return (
+//       <>      
+//       <Webcam
+//           audio={false}
+//           height={720}
+//           ref={webcamRef}
+//           screenshotFormat="image/jpeg"
+//           width={1280}
+//           videoConstraints={videoConstraints}
+//       />
+//       <button onClick={capture}>Capture photo</button>
       
-      </>
-  );
-};
+//       </>
+//   );
+// };
 
   class WebCamCapture extends Component {
     render() {
