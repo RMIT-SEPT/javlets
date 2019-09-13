909ccb7323610e07ca043b8513c8a5a1dfcc6cc8
Justin Vitale <Quadcorenerd@gmail.com>
2019-09-13 12:03:09 +1000

    Update junit 4 to 5 and configured yml for back/front end to run tests

---
+++ backend/src/test/java/com/sept/javlets/MavenSunfireTest.java
@@ -0,0 +1,11 @@
+package com.sept.javlets;
+
+import org.junit.jupiter.api.Test;
+
+class MavenExampleTest {
+
+    @Test
+    void shouldRun() {
+        System.out.println("Sunfire has found this test");
+    }
+}
