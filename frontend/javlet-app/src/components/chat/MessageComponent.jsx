30dbb657954b4c773a7f4267a62a73c4678eaf52
Chanboth Som <schanboth@hotmail.com>
2019-09-19 11:23:30 +1000

    Can't compile with stomp

--- frontend/javlet-app/src/components/chat/MessageComponent.jsx
+++ frontend/javlet-app/src/components/chat/MessageComponent.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import SockJS from "sockjs-client"
-import Stomp from "@stomp/stompjs"
+import Stomp from "stomp/stompjs"
 
 class MessageComponent extends Component{
     state = {  }
