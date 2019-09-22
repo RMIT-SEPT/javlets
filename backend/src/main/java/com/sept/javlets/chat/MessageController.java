876d6b83a890bb95d8c484ce10ef16a7e559ceff
Justin Vitale <Quadcorenerd@gmail.com>
2019-09-22 15:26:37 +1000

    Remove unused import

--- backend/src/main/java/com/sept/javlets/chat/MessageController.java
+++ backend/src/main/java/com/sept/javlets/chat/MessageController.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
