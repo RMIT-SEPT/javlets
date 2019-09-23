3307029d8a9cb3327822cdd9b6c43d64b4c94f44
Justin Vitale <Quadcorenerd@gmail.com>
2019-09-24 00:40:14 +1000

    Correct datetime spelling #deployfront

--- frontend/javlet-app/src/components/chat/MessageComponent.jsx
+++ frontend/javlet-app/src/components/chat/MessageComponent.jsx
@@ -5,7 +5,7 @@ class MessageComponent extends Component{
 render(){
    return(
     <div className="chat">
-        <h4>{this.props.sender} | {this.props.time}</h4>
+        <h4>{this.props.sender} | {this.props.datetime}</h4>
       <p> <img className='w3-circle' alt="" src={'https://img.icons8.com/cotton/64/000000/name--v2.png'} />{this.props.message}</p>
     </div>
    );
