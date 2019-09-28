6386962fcb69fb25336c4f085b3f5fd8b4f1c70c
Joseph Williams <s3569541@student.rmit.edu.au>
2019-09-28 12:51:12 +1000

    live stream chat form now clears when u post

--- frontend/javlet-app/src/components/liveStream/LiveChatCommenting.jsx
+++ frontend/javlet-app/src/components/liveStream/LiveChatCommenting.jsx
@@ -18,29 +18,28 @@ class LiveChatCommenting extends Component {
     render() {
       return (
         <div>
-        <div>        
-          <form onSubmit={this.handleSubmit}>            
-            <label> Title </label> <br />
-            <input onChange={this.handleTitleChange} value={this.state.title}/>
-            <br />
-            
-            <label> Comment </label>
-            <br />
-            <input onChange={this.handleBodyChange} value={this.state.body} />
-            <br />
-            
-            <input className="w3-btn w3-blue" type="submit" value="Submit" />
-          </form>
-        </div>
+          <div>        
+            <form id="form" onSubmit={this.handleSubmit} >            
+              <label> Title </label> <br />
+              <input onChange={this.handleTitleChange} value={this.state.title}/>
+              <br />
+              
+              <label> Comment </label>
+              <br />
+              <input onChange={this.handleBodyChange} value={this.state.body} />
+              <br />
+              <input className="w3-btn w3-blue" type="submit" value="Submit" />
+            </form>
+          </div>
 
-        <div>
-          {this.state.posts.reverse().map(item => (  
-            <div className="post" key={item.id}>
-              <h2>{item.title}</h2>
-              <p>{item.body}</p>
-            </div>
-          ))}
-      </div>
+          <div>
+            {this.state.posts.reverse().map(item => (  
+              <div className="post" key={item.id}>
+                <h2>{item.title}</h2>
+                <p>{item.body}</p>
+              </div>
+            ))}
+        </div>
       </div>
       );
     }
@@ -55,6 +54,7 @@ class LiveChatCommenting extends Component {
   
     handleSubmit(event) {
       event.preventDefault();
+      document.getElementById("form").reset();
       if (!this.state.title.length) {
         return;
       }
@@ -64,6 +64,8 @@ class LiveChatCommenting extends Component {
         id: Date.now()
       };
       this.setState( {posts: this.state.posts.concat(newItem)})
+      this.setState({title: "", body: ""});
+      
     }
   }
 
