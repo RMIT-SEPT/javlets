52c230db383d237f669c2a34adf132a702fb6763
“s3569541” <s3569541@student.rmit.edu.au>
2019-10-13 14:24:07 +1100

    changed constants back to server

--- frontend/javlet-app/src/Constants.js
+++ frontend/javlet-app/src/Constants.js
@@ -1,5 +1,5 @@
-// const API = 'http://javlet.social:8080'
-const API = 'http://localhost:8080'
+const API = 'http://javlet.social:8080'
+// const API = 'http://localhost:8080'
 
 // const API_WS = 'ws://javlet.social'
 const API_WS = 'ws://localhost'
