e27cc0527aae743ddac0bd54612afbeb8f587123
Justin Vitale <Quadcorenerd@gmail.com>
2019-10-20 12:16:10 +1100

    Connection type tests

--- frontend/javlet-app/src/test/ChatComponent.test.jsx
+++ frontend/javlet-app/src/test/ChatComponent.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme'
+import { shallow } from 'enzyme'
 import ChatComponent from '../components/chat/ChatComponent';
 
 
@@ -7,15 +7,4 @@ it('Chat SNAPSHOT', () => {
     const component = shallow(<ChatComponent />);
   
     expect(component).toMatchSnapshot();
-  });
-it('Send chat message', () => {
-    //const component = mount(<ChatComponent />);
-    // component
-    //   .find('button#my-button-one')
-    //   .simulate('keydown', { keyCode: 32 });
-
-    //   const input = component.find('input');
-
-    // expect(component).toMatchSnapshot();
-    // component.unmount();
   });
